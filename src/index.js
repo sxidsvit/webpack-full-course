6453e15053fc1d45e93bea78f4dbcc9c0645b515
unknown <s.p.antonyuk@gmail.com>
2020-01-26 14:23:43 +0200

    file-loader for fonts

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import Post from './Post'
+import './styles/styles.css'
+import json from './assets/json'
+import WebpackLogo from './assets/webpack-logo.png'
+
+const post = new Post('Webpack Post Title', WebpackLogo)
+
+console.log('Post to String: ', post.toString());
+console.log('json: ', json);
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: {
+    main: './index.js',
+    analytics: './analitics.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jp?g|gif|svg)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        use: ['file-loader']
+      }
+    ]
+  }
+}
