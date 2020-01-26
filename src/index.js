3ca0ecf75141b4c5d2a86b456d624c1b9cdcdeea
unknown <s.p.antonyuk@gmail.com>
2020-01-26 15:07:57 +0200

    xml-loader

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import Post from './Post'
-import './styles/styles.css'
 import json from './assets/json'
+import xml from './assets/data.xml'
 import WebpackLogo from './assets/webpack-logo.png'
+import './styles/styles.css'
 
 const post = new Post('Webpack Post Title', WebpackLogo)
 
 console.log('Post to String: ', post.toString());
-console.log('json: ', json);
+console.log('JSON: ', json);
+console.log('XML: ', xml.email.body[0]);
